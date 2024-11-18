d3a5d35273a1fa986a074fc5d141a91508aa32e1
cdev2024 <creativedev2024@gmail.com>
2024-11-18 10:35:26 +0900

    [2024-11-18] user 도메인 기본 패키지 설정

---
+++ api/src/main/java/com/delivery/api/domain/user/business/UserBusiness.java
@@ -0,0 +1,9 @@
+package com.delivery.api.domain.user.business;
+
+import com.delivery.api.common.annotaion.Business;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Business
+public class UserBusiness {
+}
