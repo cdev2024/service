93f1a60c864ec523ac38540629bbd7e5f3c0c4a2
cdev2024 <creativedev2024@gmail.com>
2024-11-18 09:41:16 +0900

    [2024-11-18] UserEntiti작성

---
+++ db/src/main/java/com/delivery/db/user/UserEntity.java
@@ -0,0 +1,42 @@
+package com.delivery.db.user;
+
+import com.delivery.db.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user")
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class UserEntity extends BaseEntity {
+
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Column(length = 100, nullable = false)
+    private String email;
+
+    @Column(length = 100, nullable = false)
+    private String password;
+
+    @Column(length = 50, nullable = false)
+    private String status;
+
+    @Column(length = 150, nullable = false)
+    private String address;
+
+    private LocalDateTime registeredAt;
+    private LocalDateTime unregisteredAt;
+    private LocalDateTime lastLoginAt;
+}
