734dd821431ca77ea39184660a210ab87c677c34
cdev2024 <creativedev2024@gmail.com>
2024-11-25 09:53:43 +0900

    [2024-11-25] Store 서비스 로직 개발: Service, DTO, Converter, Business 클래스 구현

--- api/src/main/java/com/delivery/api/domain/store/controller/model/StoreRegisterRequest.java
+++ api/src/main/java/com/delivery/api/domain/store/controller/model/StoreRegisterRequest.java
@@ -1,11 +1,27 @@
 package com.delivery.api.domain.store.controller.model;
 
+import com.delivery.db.store.enums.StoreCategory;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class StoreRegisterRequest {
+    private String name;
+
+    private String address;
+
+    private StoreCategory storeCategory;
+
+    private String thumbnailUrl;
+
+    private BigDecimal minimumAmount;
+
+    private BigDecimal minimumDeliveryAmount;
+
+    private String phoneNumber;
 }
