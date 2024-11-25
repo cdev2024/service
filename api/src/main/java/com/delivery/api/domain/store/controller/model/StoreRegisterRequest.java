586385fb08ed5f35a2028affb0baba4ca78a1b4a
cdev2024 <creativedev2024@gmail.com>
2024-11-25 11:05:43 +0900

    [2024-11-25] Store 서비스 로직 개발 + Store Menu 데이터베이스 개발
    - Store 서비스 로직 : StoreApiController, StoreOpenApiController 작성
    - Store Menu db 모듈 작업 : StoreMenuEntity, StoreMenuRepository 작성

--- api/src/main/java/com/delivery/api/domain/store/controller/model/StoreRegisterRequest.java
+++ api/src/main/java/com/delivery/api/domain/store/controller/model/StoreRegisterRequest.java
@@ -1,6 +1,8 @@
 package com.delivery.api.domain.store.controller.model;
 
 import com.delivery.db.store.enums.StoreCategory;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,17 +13,25 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 public class StoreRegisterRequest {
+
+    @NotBlank
     private String name;
 
+    @NotBlank // 문자열 체크, null, "", "   "
     private String address;
 
+    @NotNull
     private StoreCategory storeCategory;
 
+    @NotBlank
     private String thumbnailUrl;
 
+    @NotNull
     private BigDecimal minimumAmount;
 
+    @NotNull
     private BigDecimal minimumDeliveryAmount;
 
+    @NotNull
     private String phoneNumber;
 }
